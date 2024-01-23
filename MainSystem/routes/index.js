8ac41a1f03bde80bf9eee1b2c527070f51d354ff
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2024-01-23 07:39:49 +0700

    Merge filter from main

--- MainSystem/routes/index.js
+++ MainSystem/routes/index.js
@@ -1,21 +1,15 @@
 const homeRouter = require('./home');
 const categoryRouter = require('./category');
 const bookRouter=require('./book')
-<<<<<<< HEAD
 const cartRouter = require('./cart');
-=======
 const accRouter = require('./account.r')
->>>>>>> 25945f51fcb7525a31d817bfaca596cddf56ba16
 
 function route(app){
     app.use('/',homeRouter);
     app.use('/category', categoryRouter);
     app.use('/book',bookRouter);
-<<<<<<< HEAD
     app.use('/cart', cartRouter);
-=======
     app.use('/account', accRouter);
->>>>>>> 25945f51fcb7525a31d817bfaca596cddf56ba16
 }
 
 module.exports=route;
