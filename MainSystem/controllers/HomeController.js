448f25939810b2cb2130352ffec5b5318d8fbe06
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2024-01-24 11:19:47 +0700

    Finish Upload Profile

--- MainSystem/controllers/HomeController.js
+++ MainSystem/controllers/HomeController.js
@@ -4,7 +4,7 @@ const Account= require('../models/account.m');
 const { use } = require('passport');
 const jwt = require('jsonwebtoken')
 const Category=require('../models/Category')
-
+const fs = require('fs');
 
 class HomeController {
     async index(req, res, next) {
@@ -132,20 +132,30 @@ class HomeController {
             return res.redirect('/');
         }
 
-        let fullname, birthday, email, gender, address, avatar
-        const customer = await Account.findCustomer(account.ID);
-        fullname = customer.fullname;
-        birthday = new Date(customer.dob);
-        var year = birthday.getFullYear();
-        var month = birthday.getMonth() + 1; 
-        var day = birthday.getDate();
-        birthday = `${day < 10 ? '0' : ''}${day}-${month < 10 ? '0' : ''}${month}-${year}`;
-        email = customer.email;
-        gender = customer.gender;
-        address = customer.address;
-        avatar = customer.avatar;
+        const { fullname, birthday, email, gender, address } = req.body;
+        if (!req.file) {
+            await db.update('Customer', 'fullname', fullname, 'customer_id', account.ID);
+            await db.update('Customer', 'dob', birthday, 'customer_id', account.ID);
+            await db.update('Customer', 'email', email, 'customer_id', account.ID);
+            await db.update('Customer', 'gender', gender, 'customer_id', account.ID); 
+            await db.update('Customer', 'address', address, 'customer_id', account.ID);
+        
+            res.redirect('/profile');
+        }
+        else {
+            let avatarPath = req.file.path; 
+            const avatarPathWithForwardSlash = avatarPath.replace(/\\/g, '/');
+            const relativePath = avatarPathWithForwardSlash.split('public/')[1];
 
-        res.render('profilepage', {fullname, birthday, email, gender, address, avatar, user:account, role: role });    
+            await db.update('Customer', 'fullname', fullname, 'customer_id', account.ID);
+            await db.update('Customer', 'dob', birthday, 'customer_id', account.ID);
+            await db.update('Customer', 'email', email, 'customer_id', account.ID);
+            await db.update('Customer', 'gender', gender, 'customer_id', account.ID); 
+            await db.update('Customer', 'address', address, 'customer_id', account.ID);
+            await db.update('Customer', 'avatar', relativePath, 'customer_id', account.ID);
+
+            res.redirect('/profile');
+        }
     }
 }
 
--- MainSystem/routes/home.js
+++ MainSystem/routes/home.js
@@ -1,5 +1,49 @@
 const express=require('express');
 const route=express.Router();
+const multer = require('multer');
+const fs = require('fs');
+const Account= require('../models/account.m');
+const jwt = require('jsonwebtoken')
+const storage = multer.diskStorage({
+    destination: async (req, file, cb) => {
+      try {
+        let username;
+        const token = req.cookies.token;
+
+        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: "Token không hợp lệ." });
+            } else {
+                username = decoded.username;
+            }
+        });
+        const account = await Account.findAccount(username);
+        if (!account) {
+            res.clearCookie('token');
+            return res.redirect('/');
+        }
+
+        const destinationFolder = `MainSystem/public/Images/Customers/${account.ID}`;
+  
+        fs.mkdir(destinationFolder, { recursive: true }, (err) => {
+          if (err) {
+            console.error(err);
+            cb(err, null);
+          } else {
+            cb(null, destinationFolder);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        cb(error, null);
+      }
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+});
+  
+const uploadImg = multer({ storage: storage });
 
 const cookieParser = require("cookie-parser");
 
@@ -12,7 +56,10 @@ route.get('/profile',HomeController.profile);
 route.get('/',HomeController.index);
 
 route.post('/profile/edit',HomeController.profileEdit);
-route.post('/profile/upload',HomeController.profileUpload);
+route.post('/profile/upload', uploadImg.single('avatar'), (req, res, next) => {
+    console.log("File uploaded successfully");
+    next();
+}, HomeController.profileUpload);
 
 
 module.exports=route;
